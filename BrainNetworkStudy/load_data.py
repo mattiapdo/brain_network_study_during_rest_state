987f1f82e01487ff325de1bf5117bf855b90eb4e
mattiapdo <mattiapdo@gmail.com>
2018-12-26 18:32:51 +0100

    first point - wik

--- BrainNetworkStudy/load_data.py
+++ BrainNetworkStudy/load_data.py
@@ -7,16 +7,38 @@ Created on Tue Dec 18 23:42:01 2018
 
 import pyedflib
 import mylib 
-#import pandas as pd
+import pandas as pd
+import mvar
+import numpy as np
+
+#%% Load data and store into a dataframe
 
 f = pyedflib.EdfReader('../data/S001R01.edf')
-
 # mylib.print_some_stuff(f)
 
-
 data = mylib.edfToDataFrame(f)
-
-
-
-
 f._close()
+
+#%%
+
+data = data.T.values
+N, n = data.shape
+p = 3
+A_est, sigma = mvar.mvar_fit(data, p)
+sigma = np.diag(sigma)  # DTF + PDC support diagonal noise
+# sigma = None
+
+# compute DTF
+D, freqs = mvar.DTF(A_est, sigma)
+#mvar.plot_all(freqs, D, 'DTF') it takes a lot to run this
+
+# compute PDC
+P, freqs = mvar.PDC(A_est, sigma)
+#plot_all(freqs, P, 'PDC') it takes a lot to run this
+
+D = D.max(axis = 0).shape
+P = P.max(axis = 0).shape
+
+import matplotlib.pyplot as plt
+
+plt.hist(freqs)
