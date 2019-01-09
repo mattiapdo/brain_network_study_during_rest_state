4dfe480304a583d049a5092b3ae1458ca2841b52
mattiapdo <mattiapdo@gmail.com>
2019-01-09 01:37:08 +0100

    improved user experience

--- main.py
+++ main.py
@@ -16,8 +16,9 @@ import matplotlib.pyplot as plt
 import re
 import igraph
 #%% Load data and store into a dataframe
-
-f = pyedflib.EdfReader('./data/S001R01.edf')
+path = './data/S001R01.edf'
+print("Loading data from", path)
+f = pyedflib.EdfReader(path)
 # mylib.print_some_stuff(f)
 
 data = mylib.edfToDataFrame(f)
@@ -33,13 +34,16 @@ p = 3
 A_est, sigma = mvar.mvar_fit(measures, p)
 sigma = np.diag(sigma)  # The noise for each time series
 # compute DTF
+print("Computing DFT")
 D, freqs = mvar.DTF(A_est, sigma, T)
 # compute PDC
+print("Computing PDC")
 # P, freqs = mvar.PDC(A_est, sigma)
 #%%
 # take take a graph relative to a specific frequency
 freq = 10. # this is in Hz and roughly corresponds to Alpha (mu) ERD
 #freq = 28   # this is in Hz and roughly corresponds to Beta ERS
+print("Building network to analyse interactions at", freq, "Hz")
 # select the correspondent weights
 D = D[np.where(freqs == mylib.find_nearest(freqs, freq)),:,:].reshape(64, 64)
 G = Graph.Weighted_Adjacency(D.tolist(), mode = 0) # mode=0 is for directed / mode=1 is for indirected graph
@@ -47,24 +51,26 @@ G = Graph.Weighted_Adjacency(D.tolist(), mode = 0) # mode=0 is for directed / mo
 # get channel names, cleaning replacing any dot with a void charachter
 G.vs["label"] = list(map(lambda x: re.sub('\.', '', x), data.columns.values))
 # %%
-G = mygraph.applyTreshold(G, 0.05)
+threshold = 0.05
+print("Applying a threshold on network density of", round(threshold*100), "%")
+G = mygraph.applyTreshold(G, threshold)
 
 #%%
 G = mygraph.add_brain_layout(G)
 
 #%%
-
+print("Making a topological representation")
 visual_style = {}
 visual_style["vertex_size"] = 20
 visual_style["vertex_color"] = "white"
-visual_style["vertex_label"] = G2.vs["label"]
+visual_style["vertex_label"] = G.vs["label"]
 visual_style["edge_width"] = [1 + 2 * int(weight) for weight in G.es["weight"]]
 visual_style["layout"] = G.vs["coordinates"]
 igraph.plot(G, **visual_style)
 
 #%%
 # 2.1
-
+print("Macroscopic Network Analysis")
 # global clustering coefficient
 # https://igraph.org/python/doc/igraph.Graph-class.html#transitivity_avglocal_undirected
 print("global clustering coefficient = ", round(G.transitivity_avglocal_undirected(), 3))
@@ -73,39 +79,40 @@ print("global clustering coefficient = ", round(G.transitivity_avglocal_undirect
 # https://igraph.org/python/doc/igraph.GraphBase-class.html#average_path_length
 print("global average path lenght = ", round(G.average_path_length(directed = False, unconn=True), 3))
 
+print("Microscopic Network Analysis")
 local_ind = pd.DataFrame.from_dict(      {
-                "channel" : G.vs["names"] ,
-                "strenght" : G.strength(mode = 3),
-                "out-strength" : G.strength(mode = 1),
-                "in-strength" : G.strength(mode = 2)
+                "channel" : G.vs["label"] ,
+                "degree" : G.strength(mode = 3),
+                "out-degree" : G.strength(mode = 1),
+                "in-degree" : G.strength(mode = 2)
                 }
         )
 
 local_ind.set_index("channel", inplace = True)
-print("\nTop 10 channels by degree\n", local_ind.sort_values(by = "strenght", ascending = False)[1:10])
-print("\nTop 10 channels by OUT degree\n", local_ind.sort_values(by = "out-strength", ascending = False)[1:10])
-print("\nTop 10 channels by IN degree\n", local_ind.sort_values(by = "in-strength", ascending = False)[1:10])
+print("Top 10 channels by degree\n", local_ind.sort_values(by = "degree", ascending = False)[1:10]["degree"])
+print("Top 10 channels by OUT degree\n", local_ind.sort_values(by = "out-degree", ascending = False)[1:10]["out-degree"])
+print("Top 10 channels by IN degree\n", local_ind.sort_values(by = "in-degree", ascending = False)[1:10]["in-degree"])
 
 
 
 # 2.7
 local_ind = pd.DataFrame.from_dict(      {
-                "channel" : G.vs["names"] ,
-                "strenght" : G.strength(mode = 3,  weights = "weight"),
+                "channel" : G.vs["label"] ,
+                "strength" : G.strength(mode = 3,  weights = "weight"),
                 "out-strength" : G.strength(mode = 1,  weights = "weight"),
                 "in-strength" : G.strength(mode = 2,  weights = "weight")
                 }
         )
 
 local_ind.set_index("channel", inplace = True)
-#print("\nTop 10 channels by generalized degree\n", local_ind.sort_values(by = "strenght", ascending = False)[1:10])
-#print("\nTop 10 channels by generalized OUT degree\n", local_ind.sort_values(by = "out-strength", ascending = False)[1:10])
-#print("\nTop 10 channels by generalized IN degree\n", local_ind.sort_values(by = "in-strength", ascending = False)[1:10])
+print("Top 10 channels by generalized degree\n", local_ind.sort_values(by = "strength", ascending = False)[1:10]["strength"])
+print("Top 10 channels by generalized OUT degree\n", local_ind.sort_values(by = "out-strength", ascending = False)[1:10]["out-strength"])
+print("Top 10 channels by generalized IN degree\n", local_ind.sort_values(by = "in-strength", ascending = False)[1:10]["in-strength"])
 
 #%%
 # 2.4 Study the behaviour of global graph indices in function of network density
 print("Studying the behaviour of global graph indices in function of network density...")
-densities = [i*0.1 for i in range(1,10)] # edit these with the ones provided by the prof
+densities = [0.01, 0.05, 0.1, 0.2, 0.3, 0.5] # edit these with the ones provided by the prof
 # these two lines take some seconds...
 clustering_coeffs = [mygraph.applyTreshold(Graph.Weighted_Adjacency(D.tolist(), mode = 0), i).transitivity_avglocal_undirected() for i in densities]
 average_path_lengths = [mygraph.applyTreshold(Graph.Weighted_Adjacency(D.tolist(), mode = 0), i).average_path_length(directed = False, unconn=True) for i in densities]
@@ -137,6 +144,8 @@ def write_inputFileForMotifAnalysis(G, file):
                     file.write(str(source) + "  " + str(target) +"  " + str(1)+"\n")
     file.close()
     return
-
-
-write_inputFileForMotifAnalysis(G, file = './data/inputForMA.txt')
+    
+path = './data/inputForMA.txt'
+print("Writing input file for Motif Analysis to", path)
+write_inputFileForMotifAnalysis(G, file = path)
+print("\nDone")
--- mygraph.py
+++ mygraph.py
@@ -30,5 +30,4 @@ def add_brain_layout(g):
     chan_loc = pd.read_csv("./data/channels_topology.csv", sep=';', decimal=',')
     pos = {row[0]: (row[1],row[2]) for row in chan_loc.values}
     g.vs["coordinates"] = [pos[i["label"]] for i in g.vs]
-    igraph.plot(g, pos = g.vs["coordinates"], vertex_label = g.vs["label"])
     return(g)
