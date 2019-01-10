8472ad28e05f471d671ee0a905ddc89f80aa3b62
mattiapdo <mattiapdo@gmail.com>
2019-01-10 01:59:36 +0100

    delete some stuff

--- positions.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Jan  8 18:56:36 2019
-
-@author: Mattia
-"""
-
-
--- smallWorld.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Jan  9 19:20:17 2019
-
-@author: Mattia
-"""
-
-
-
